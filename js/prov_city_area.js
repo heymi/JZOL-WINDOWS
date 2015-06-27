0c1353eb4ba88a159933d3a87d2b41c6c77068d0
Leeiio <guaniu@gmail.com>
2015-06-27 15:13:18 +0800

    新增阿姨改进籍贯和现居地址选择，改进属相选择

--- js/prov_city_area.js
+++ js/prov_city_area.js
@@ -81,9 +81,9 @@ $.province_city_area = function(_province,_city,_area,_address){
   var DATA_PROV = PROVCITYAREA.province;
   var DATA_CITY = PROVCITYAREA.cityData;
   var DATA_AREA = PROVCITYAREA.areaData;
-  var prov_str = DATA_PROV[_province - 1].name + '省' || '';
-  var city_str = DATA_CITY[_city - 1].name || '';
-  var area_str = DATA_AREA[_area - 1].name || '';
+  var prov_str = _province !== 0 ? DATA_PROV[_province - 1].name + '省' || '' : '';
+  var city_str = _city !== 0 ? DATA_CITY[_city - 1].name || '' : '';
+  var area_str = _area !== 0 ? DATA_AREA[_area - 1].name || '' : '';
   var address = _address || '';
   return prov_str + city_str + area_str + address;
 };
