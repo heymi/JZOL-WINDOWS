26576c4a3c3358a3609a3ed15da836d598c9facc
Leeiio <guaniu@gmail.com>
2015-06-28 15:36:20 +0800

    调整窗口的自适应宽和高

--- js/init.js
+++ js/init.js
@@ -4,4 +4,8 @@
 
 $(function () {
   //$('.container').height($(window).height());
+  if (window.top != window.self) {
+    var paddingHeight = parseInt($('.container').css('padding-top').replace('px', ''),10) + parseInt($('.container').css('padding-bottom').replace('px', ''),10);
+    $('.container').height($(window).height() - paddingHeight);
+  }
 });
