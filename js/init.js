75d79ab0cad41e880fa3f2f6d9e51b7ac93b848d
Leeiio <guaniu@gmail.com>
2015-06-29 15:10:43 +0800

    检测登录失效

--- js/init.js
+++ js/init.js
@@ -3,6 +3,14 @@
  */
 
 $(function () {
+  $.ajaxSetup({
+    complete: function(xhr, status){
+      var code = JSON.parse(xhr.responseText).code;
+      if(code === -1){
+        $.scojs_message('登录失效，请退出后请重新登录！', $.scojs_message.TYPE_ERROR);
+      }
+    }
+  });
   //$('.container').height($(window).height());
   if (window.top != window.self) {
     var paddingHeight = parseInt($('.container').css('padding-top').replace('px', ''),10) + parseInt($('.container').css('padding-bottom').replace('px', ''),10);
